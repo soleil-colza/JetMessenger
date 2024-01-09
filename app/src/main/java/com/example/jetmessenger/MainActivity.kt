67429d27e9b01f7d99652e541fb00c622ed1bbb1
Hina Nakahira <soleil.colza@icloud.com>
2024-01-10 02:47:41 +0900

    created the TextField and FAB's UI initially

--- app/src/main/java/com/example/jetmessenger/MainActivity.kt
+++ app/src/main/java/com/example/jetmessenger/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.jetmessenger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetmessenger.ui.theme.JetMessengerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +36,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    UserInputScreen()
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserInputScreen() {
+
+    val textState = remember { mutableStateOf("") }
+
+    Column {
+        TextField(
+            value = textState.value,
+            onValueChange = { newText:String ->
+                textState.value = newText
+            }
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        FloatingActionButton(onClick = {
+            // Submit textState.value
+        }) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "Send"
+            )
+        }
+    }
+
 }
 
