a0486806c4c00cf83cb487a027a83c5a170bf388
Hina Nakahira <soleil.colza@icloud.com>
2024-01-10 17:58:51 +0900

    rewrote the UI components using the Scaffold component

--- app/src/main/java/com/example/jetmessenger/MainActivity.kt
+++ app/src/main/java/com/example/jetmessenger/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.jetmessenger
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.materialIcon
@@ -14,6 +18,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,31 +49,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserInputScreen() {
 
     val textState = remember { mutableStateOf("") }
 
-    Column {
-        TextField(
-            value = textState.value,
-            onValueChange = { newText:String ->
-                textState.value = newText
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /* FABのクリック処理 */ }) {
+                Icon(imageVector = Icons.Default.Send, contentDescription = "Send")
             }
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        FloatingActionButton(onClick = {
-            // Submit textState.value
-        }) {
-            Icon(
-                imageVector = Icons.Default.Send,
-                contentDescription = "Send"
-            )
         }
+    ) {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                TextField(
+                    value = textState.value,
+                    onValueChange = { textState.value = it }
+                )
+            }
+
+        }
+
     }
 
 }
-
