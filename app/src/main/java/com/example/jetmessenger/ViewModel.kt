944203b3d58bffdb320278a7b4acd0e278d1dcbc
Hina Nakahira <soleil.colza@icloud.com>
2024-01-24 20:30:00 +0900

    デフォルトのviewModelScopeの導入

--- app/src/main/java/com/example/jetmessenger/ViewModel.kt
+++ app/src/main/java/com/example/jetmessenger/ViewModel.kt
@@ -2,9 +2,8 @@ package com.example.jetmessenger
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewModelScope
 import com.example.jetmessenger.BuildConfig.WEBHOOK_URL
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
@@ -24,21 +23,17 @@ class MainViewModel : ViewModel() {
     //MutableLiveDataを生成。これはUIの状態を保持するために使用する。
     val textState = MutableLiveData<String>()
 
-    //CoroutineScope(コルーチンのスコープを定義するもの)を生成。UIスレッドで実行するようにDispatchers.Mainを指定している。
-    private val viewModelScope = CoroutineScope(Dispatchers.Main)
-
     //sendMessageメソッドで, viewModelScopeを使ってコルーチンを発行。IOスレッドでバックグラウンド処理を実行する。
     fun sendMessage() {
         viewModelScope.launch(Dispatchers.IO) {
-
             val rawText = textState.value
             var message: discordMessage? = null
-
             if (!rawText.isNullOrEmpty()) {
 
                 // textStateの値からDiscordMessageインスタンスを生成。
                 //　Retrofitを通してAPIを叩き、WebhookApiに渡して送信。
                 message = discordMessage(rawText)
+                discordWebhook.sendMessage(message)
             }
 
             // ?.はSafe Call, letもnullでなければalsoの処理を実行する。
