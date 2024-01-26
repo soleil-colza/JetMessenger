5b2c76b0f17a7c784418f894ddd0275e80a791f7
Hina Nakahira <soleil.colza@icloud.com>
2024-01-26 13:56:04 +0900

    定数名の修正

--- app/src/main/java/com/example/jetmessenger/ViewModel.kt
+++ app/src/main/java/com/example/jetmessenger/ViewModel.kt
@@ -19,17 +19,15 @@ class MainViewModel : ViewModel() {
     public val textState = MutableLiveData("")
 
     //RetrofitインスタンスからWebhookApiのインスタンスを生成。インターフェイスの実装が動的に生成される。
-    val DiscordWebhook = retrofit.create(DiscordWebhook::class.java)
+    val discordWebhook = retrofit.create(DiscordWebhook::class.java)
 
     //sendMessageメソッドで, viewModelScopeを使ってコルーチンを発行。IOスレッドでバックグラウンド処理を実行する。
     fun sendMessage() {
         viewModelScope.launch(Dispatchers.IO) {
             val rawText = textState.value
-            var message: DiscordMessage? = null
-
             textState.value?.let { text ->
                 // textStateの値がnullでない場合、DiscordMessageを生成してWebhookに送信
-                DiscordWebhook.sendMessage(DiscordMessage(text))
+                discordWebhook.sendMessage(DiscordMessage(text))
             }
         }
     }
