fb83d1fc36b22711a28e7d7780a2aa9fa0a1101d
Hina Nakahira <soleil.colza@icloud.com>
2024-02-06 12:57:20 +0900

    JSONアノテーションの削除

--- app/src/main/java/com/example/jetmessenger/data/DiscordMessage.kt
+++ app/src/main/java/com/example/jetmessenger/data/DiscordMessage.kt
@@ -1,8 +1,5 @@
 package com.example.jetmessenger.data
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
 data class DiscordMessage(
-    @field:Json(name = "content") val content: String
+    val content: String
 )
