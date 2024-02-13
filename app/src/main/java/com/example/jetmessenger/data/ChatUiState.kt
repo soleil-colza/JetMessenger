bbb571d6a54025d26113f4dc83a634f071800444
Hina Nakahira <soleil.colza@icloud.com>
2024-02-14 01:40:51 +0900

    isBooleanをChatUiStateに追加

--- app/src/main/java/com/example/jetmessenger/data/ChatUiState.kt
+++ app/src/main/java/com/example/jetmessenger/data/ChatUiState.kt
@@ -2,5 +2,6 @@ package com.example.jetmessenger.data
 
 data class ChatUiState(
     val inputText: String = "",
-    val messages: Array<ReceivedMessage> = emptyArray()
+    val messages: Array<ReceivedMessage> = emptyArray(),
+    val isLoading : Boolean
 )
